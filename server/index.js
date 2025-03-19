c8092a5872b9750979118bf3de2e33239f580197
msuhyeon <job.maengsh@gmail.com>
2025-03-19 19:55:33 +0900

    feat: 키워드 추출 api 추가

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
-const express = require('express');
+import express from 'express';
+import mecab from 'mecab-ya';
 const app = express();
-
 app.use(express.json());
+
+const PORT = 5000;
 
 app.get('/', (req, res) => {
   res.send('Server Start!');
+});
+
+app.post('/extract-keywords', async (req, res) => {
+  try {
+    const { text } = req.body;
+
+    // 형태소 분석
+    const result = await mecab.parse(text);
+  } catch (error) {
+    console.error(`Error in extract-keywords: ${error}`);
+    throw new Error();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log('서버 실행 중!!');
 });
