9dc25d37fae836afdc3871a027afa6873fd4fa75
msuhyeon <job.maengsh@gmail.com>
2025-03-26 18:29:40 +0900

    style: 패딩 값 수정

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   });
 
   return (
-    <header className="w-full p-4 flex justify-end">
+    <header className="w-full pt-4 flex justify-end">
       <button
         className={`${darkMode ? ' bg-white-300' : 'bg-gray-100'} py-2 px-4 rounded-md dark:bg-gray-700 transition-all cursor-pointer`}
         onClick={() => setDarkMode(!darkMode)}
