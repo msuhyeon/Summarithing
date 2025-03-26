db4c25646af4f85b2e8e4ce773a005b0aa775e5b
msuhyeon <job.maengsh@gmail.com>
2025-03-26 15:59:23 +0900

    chore: 불필요한 코드 제거

--- server/main.py
+++ server/main.py
@@ -45,7 +45,6 @@ client = OpenAI()
 client.api_key = os.getenv("OPENAI_API_KEY")
 
 def call_openai(text: str) -> str:
-    # print("STR")
     try: 
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
