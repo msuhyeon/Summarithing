942b01e495a54992d42d90f4262f9defec798cb5
msuhyeon <job.maengsh@gmail.com>
2025-03-27 17:46:03 +0900

    style: 화면 구조 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,21 +19,25 @@ const App = () => {
 
   return (
     <>
-      <div className="flex flex-col min-h-screen pl-30 pr-30">
+      <div className="flex flex-col min-h-screen  pl-30 pr-30 ">
         <Header />
-        {mode === 'input' && (
-          <div
-            className="flex-grow flex flex-col justify-center items-center transform -translate-y-30
-"
-          >
-            <h1 className="mb-4 text-3xl font-orbit-600 antialiased">요약요정: 요요</h1>
-            <h2 className="mb-10 font-sans text-neutral-700 antialiased">
-              이걸 언제 다 읽어요? 🤖✨ 요요가 핵심만 쏙쏙 요약해드릴게요!
-            </h2>
-            <FileTextInput onSendData={handleData} />
-          </div>
-        )}
-        {mode === 'result' && <SummarizingResult summaryData={summaryData} />}
+        <div
+          className="flex-grow flex flex-col justify-center items-center transform -translate-y-30
+            
+            "
+        >
+          {mode === 'input' && (
+            <>
+              {' '}
+              <h1 className="mb-4 text-3xl font-orbit-600 antialiased">요약요정: 요요</h1>
+              <h2 className="mb-10 font-sans text-neutral-700 antialiased">
+                이걸 언제 다 읽어요? 🤖✨ 요요가 핵심만 쏙쏙 요약해드릴게요!
+              </h2>
+              <FileTextInput onSendData={handleData} />
+            </>
+          )}
+          {mode === 'result' && <SummarizingResult summaryData={summaryData} />}
+        </div>
         <footer className="p-5 text-sm text-center text-gray-500">
           &copy; {new Date().getFullYear()} Suhyeon. All rights reserved.
         </footer>
